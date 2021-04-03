077f62211585b27121d8e059f80c1670888a268b
Rene Schwarz <rene-schwarz-99@protonmail.com>
2021-04-03 05:10:47 +0200

    finish login window styling and added xaml event triggers to handle ui method calls

---
+++ Server Dashboard/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server_Dashboard {
+    class LoginViewModel {
+        private string password;
+
+        public string Password {
+            get { return password; }
+            set { password = value; }
+        }
+
+    }
+}
